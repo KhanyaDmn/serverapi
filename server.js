14b4ed9cfc79b7dd03914bd4e0c2b772fdf11d7f
KhanyaDmn <105922369+KhanyaDmn@users.noreply.github.com>
2024-01-31 20:44:10 +0200

    updated

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
 const app = express();
+
+
 const PORT = 3001;
 
 app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
 
 app.get('/profile', (req, res) => {
     res.send('connected')
